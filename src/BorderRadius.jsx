3ed2362a081bbd22d93d7c3fe06026bafbf2313b
FaresMer <merfares04@gmail.com>
2025-08-03 21:25:31 +0100

    fixed distance problem

--- src/BorderRadius.jsx
+++ src/BorderRadius.jsx
@@ -28,8 +28,8 @@ function BorderRadius(){
     
         <div className="result-box">
             <input type="range" name="" id="left" onChange={(e) => setLeft(e.target.value)} />
-            <div className="result" style={{borderRadius:`${top}px ${right}px ${bottom}px ${left}px`}}>
-                <span id="toCopy">{'{'} border-radius: {top}px {right}px {bottom}px {left}px ;{'}'}</span><sub><button className="copy-button" onClick={copyToClipboard}>📋</button></sub>
+            <div className="result" style={{borderRadius:`${top}% ${right}% ${bottom}% ${left}%`}}>
+                <span id="toCopy">{'{'} border-radius: {top}% {right}% {bottom}% {left}% ;{'}'}</span><sub><button className="copy-button" onClick={copyToClipboard}>📋</button></sub>
             </div>
             <input type="range" name="" id="right" onChange={(e) => setRight(e.target.value)} />
         </div>
